735c49c5f37a4160c54cf7fadc1c576874774cf8
giladam <giladam@cs.huji.ac.il>
2019-04-13 11:45:34 +0300

    add thread class

---
+++ ex2/thread.h
@@ -0,0 +1,58 @@
+#ifndef _THREAD_H
+#define _THREAD_H
+
+enum State {READY, BLOCKED, RUNNING};
+
+#ifdef __x86_64__
+/* code for 64 bit Intel arch */
+
+typedef unsigned long address_t;
+#define JB_SP 6
+#define JB_PC 7
+
+/* A translation is required when using an address of a variable.
+   Use this as a black box in your code. */
+address_t translate_address(address_t addr)
+{
+    address_t ret;
+    asm volatile("xor    %%fs:0x30,%0\n"
+		"rol    $0x11,%0\n"
+                 : "=g" (ret)
+                 : "0" (addr));
+    return ret;
+}
+
+#else
+/* code for 32 bit Intel arch */
+
+typedef unsigned int address_t;
+#define JB_SP 4
+#define JB_PC 5 
+
+/* A translation is required when using an address of a variable.
+   Use this as a black box in your code. */
+address_t translate_address(address_t addr)
+{
+    address_t ret;
+    asm volatile("xor    %%gs:0x18,%0\n"
+		"rol    $0x9,%0\n"
+                 : "=g" (ret)
+                 : "0" (addr));
+    return ret;
+}
+
+#endif
+class Thread
+{
+ private:
+    int tid, quantums;
+    address_t sp, pc;
+    State cur_state;
+ public:
+    Thread();
+    int get_quantums();
+    void inc_quantums();
+    void set_state(State);
+}; 
+
+#endif
