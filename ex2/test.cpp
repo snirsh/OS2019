58b09e3d40e6039c40455929b12a414947204d95
giladam <giladam@cs.huji.ac.il>
2019-04-15 19:23:07 +0300

    testing

--- ex2/test.cpp
+++ ex2/test.cpp
@@ -5,6 +5,9 @@ using namespace std;
 void foo() {
     cout<<"## FOO ##"<<endl;
 }
+void bar() {
+    cout<<"## BAR ##"<<endl;
+}
 
 void wait() {
     int k;
@@ -19,12 +22,15 @@ int main(int argc, char* argv[]) {
     cout<<endl<<"############### TESTING ###############"<<endl;
     uthread_init(50000);
     uthread_spawn(&foo);
-    uthread_spawn(&foo);
+    uthread_spawn(&bar);
+    wait();
+    uthread_block(1);
     wait();
     uthread_block(2);
     wait();
-    uthread_block(2);
-
+    uthread_resume(1);
+    wait();
+    uthread_resume(2);
 
     for(;;){}
     return 0;
