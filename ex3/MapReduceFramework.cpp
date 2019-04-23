360eea74e5f29addeb4eccd4280d1cf5aee803a8
giladam <giladam@cs.huji.ac.il>
2019-04-23 17:58:15 +0300

    foo

--- ex3/MapReduceFramework.cpp
+++ ex3/MapReduceFramework.cpp
@@ -20,6 +20,7 @@ struct JobContext {
 	const InputVec* input_vec;
 	IntermediateVec* inter_vec;
 	OutputVec* output_vec;
+	pthread_mutex_t mutex1, mutex2;
 };
 struct ThreadContext {
 	int tid;
@@ -55,13 +56,13 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
 	ThreadContext t_con[multiThreadLevel];
 	Barrier barrier(multiThreadLevel);
 	JobState js = {stage_t(0),0};
+	pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+	pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
 	IntermediateVec* inter_vec = new IntermediateVec();
-	JobContext jc = {multiThreadLevel, threads, &js, &barrier, &client, &inputVec, inter_vec, &outputVec};
+	JobContext jc = {multiThreadLevel, threads, &js, &barrier, &client, &inputVec, inter_vec, &outputVec, mutex1, mutex2};
 
 	for (int i = 0; i < multiThreadLevel; ++i) {
 		t_con[i] = {i, &jc};
-	}
-	for (int i = 0; i < multiThreadLevel; ++i) {
 		pthread_create(&threads[i], NULL, do_work, &t_con[i]);
 	}
 	return &jc;
@@ -76,17 +77,23 @@ void waitForJob(JobHandle job) {
 }
 void emit2 (K2* key, V2* value, void* context)
 {
-	IntermediatePair p = new IntermediatePair(key, value);
-	pthread_mutex_lock(*context.mutex1);
-	context.inter_vec.insert(p);
-	pthread_mutex_unlock(*context.mutex1);
+	IntermediatePair p = IntermediatePair(key, value);
+	JobContext* jc = (JobContext*)context;
+	IntermediateVec vec = *(jc->inter_vec);
+	pthread_mutex_lock(&(jc->mutex1));
+	auto it = vec.begin();
+	vec.insert(it, p);
+	pthread_mutex_unlock(&(jc->mutex1));
 }
 
 void emit3 (K3* key, V3* value, void* context){
-	IntermediatePair p = new IntermediatePair(key, value);
-	pthread_mutex_lock(*context.mutex2);
-	context.output_vec.insert(p);
-	pthread_mutex_unlock(*context.mutex2);
+	OutputPair p = OutputPair(key, value);
+	JobContext* jc = (JobContext*)context;
+	OutputVec vec = *(jc->output_vec);
+	pthread_mutex_lock(&(jc->mutex2));
+	auto it = vec.begin();
+	vec.insert(it, p);
+	pthread_mutex_unlock(&(jc->mutex2));
 }
 void getJobState(JobHandle job, JobState* state);
 void closeJobHandle(JobHandle job);
