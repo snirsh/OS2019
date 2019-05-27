4b74e6570d656bc0eae31d30d1402a1f98ffb299
Gil Adam <gil.adam@intel.com>
2019-05-28 00:19:27 +0300

    fix makefile

--- ex4/VirtualMemory.cpp
+++ ex4/VirtualMemory.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
 #include "VirtualMemory.h"
 #include "PhysicalMemory.h"
+
+#define MSG(msg) std::cout << msg << std::endl;
 
 void clearTable(uint64_t frameIndex) {
     for (uint64_t i = 0; i < PAGE_SIZE; ++i) {
@@ -8,67 +11,10 @@ void clearTable(uint64_t frameIndex) {
 }
 
 void VMinitialize() {
+    MSG("PAGE_SIZE: " << PAGE_SIZE)
+    MSG("RAM_SIZE: " << RAM_SIZE)
+    MSG("VIRTUAL_MEMORY_SIZE: " << VIRTUAL_MEMORY_SIZE)
     clearTable(0);
-}
-
-
-int VMread(uint64_t virtualAddress, word_t* value) {
-    uint64_t seg, addr1;
-    word_t addr2;
-    addr1 = 0;
-    for (uint64_t i = TABLES_DEPTH ; i >= 0; i--) {
-        seg = (virtualAddress >> (OFFSET_WIDTH * i)) && ((uint64_t)(PAGE_SIZE-1));
-        if (i == 0) {
-            PMread(addr1 * PAGE_SIZE + seg, value);
-            return 1;
-        }
-        PMread(addr1 * PAGE_SIZE + seg, &addr2);
-        if (addr2 == 0) {
-            uint64_t frame = find_frame(0, addr1);
-            PMwrite(frame << 1, 0);
-            PMwrite((frame << 1)+1, 0);
-            PMwrite(addr1 * PAGE_SIZE + seg, frame);
-            addr1 = frame;
-        } else {
-            addr1 = addr2;
-        }
-    }
-    return 0;
-}
-
-
-int VMwrite(uint64_t virtualAddress, word_t value) {
-
-    uint64_t seg, addr1;
-    word_t addr2;
-    addr1 = 0;
-    for (uint64_t i = TABLES_DEPTH ; i >= 0; i--) {
-        seg = (virtualAddress >> (OFFSET_WIDTH * i)) && ((uint64_t)(PAGE_SIZE-1));
-        if (i == 0) {
-            PMwrite(addr1 * PAGE_SIZE + seg, value);
-            return 1;
-        }
-        PMread(addr1 * PAGE_SIZE + seg, &addr2);
-        if (addr2 == 0) {
-            uint64_t frame = find_frame(0, addr1);
-            PMwrite(frame << 1, 0);
-            PMwrite((frame << 1)+1, 0);
-            PMwrite(addr1 * PAGE_SIZE + seg, frame);
-            addr1 = frame;
-        } else {
-            addr1 = addr2;
-        }
-    }
-    return 0;
-}
-
-uint64_t find_frame(uint64_t index, uint64_t ignore) {
-    uint64_t ret = rec_helper(index, ignore);
-    if (ret % 2 == 0) {
-        return ret >> 1;
-    } else {
-        return (ret >> 1) + 1;
-    }
 }
 
 uint64_t rec_helper(uint64_t index, uint64_t ignore) {
@@ -78,7 +24,7 @@ uint64_t rec_helper(uint64_t index, uint64_t ignore) {
 
     PMread(index << 1, &w1);
     PMread((index << 1) + 1, &w2);
-    if (!(w1 && w2) && index != ignore) {
+    if (!((w1 && w2) && (index != ignore))) {
         return index << 1;
     }
 
@@ -93,3 +39,57 @@ uint64_t rec_helper(uint64_t index, uint64_t ignore) {
         return ret1 > ret2 ? (ret1 << 1) + 1  : (ret2 << 1) + 1;
     }
 }
+
+uint64_t find_frame(uint64_t index, uint64_t ignore) {
+    uint64_t ret = rec_helper(index, ignore);
+    if (ret % 2 == 0) {
+        return ret >> 1;
+    } else {
+        return (ret >> 1) + 1;
+    }
+}
+
+int load_page(uint64_t virtualAddress) {
+    uint64_t seg, addr1;
+    word_t addr2;
+    addr1 = 0;
+    for (uint64_t i = TABLES_DEPTH ; i >= 0; --i) {
+        seg = (virtualAddress >> (OFFSET_WIDTH * i)) && ((uint64_t)(PAGE_SIZE-1));
+        if (i == 0) {
+            if (addr1 != addr2) {
+                PMrestore(addr1, virtualAddress);
+            }
+            return addr1;
+        }
+        PMread(addr1 * PAGE_SIZE + seg, &addr2);
+        if (addr2 == 0) {
+            uint64_t frame = find_frame(0, addr1);
+            PMwrite(frame << 1, 0);
+            PMwrite((frame << 1)+1, 0);
+            PMwrite(addr1 * PAGE_SIZE + seg, frame);
+            addr1 = frame;
+        } else {
+            addr1 = addr2;
+        }
+    }
+}
+
+int VMread(uint64_t virtualAddress, word_t* value) {
+    word_t frame;
+    uint64_t offset;
+    offset = virtualAddress && ((uint64_t)(PAGE_SIZE-1));
+    frame = load_page(virtualAddress);
+    PMread(frame * PAGE_SIZE + offset, value);
+    return 1;
+}
+
+int VMwrite(uint64_t virtualAddress, word_t value) {
+    word_t frame;
+    uint64_t offset;
+    offset = virtualAddress && ((uint64_t)(PAGE_SIZE-1));
+    frame = load_page(virtualAddress);
+    PMwrite(frame * PAGE_SIZE + offset, value);
+    return 1;
+}
+
+
