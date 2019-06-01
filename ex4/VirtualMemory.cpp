0a321dd3a24689c7070328e6f4e011bcf26a31a9
giladam <giladam@cs.huji.ac.il>
2019-06-01 17:11:36 +0300

    ex4 dbg session

--- ex4/VirtualMemory.cpp
+++ ex4/VirtualMemory.cpp
@@ -30,7 +30,7 @@ void VMinitialize() {
 }
 
 frame_wrapper rec_helper(word_t index, word_t ignore) {
-    usleep(200);
+    usleep(2000);
     word_t w, max=0;
     frame_wrapper ret = frame_wrapper({0, EMPTY});
     
